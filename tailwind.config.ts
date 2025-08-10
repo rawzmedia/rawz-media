fd73d48dd57f4d40a41a00d9c15cd8adb27b1892
rawzmedia <cimzbmedia@gmail.com>
2025-08-10 19:44:34 -0400

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,18 @@
 import type { Config } from "tailwindcss";
+
 const config: Config = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: { extend: {} },
+  theme: {
+    extend: {
+      fontFamily: {
+        eurostile: ['"Eurostile Extended"', 'sans-serif'],
+      },
+    },
+  },
   plugins: [],
 };
+
 export default config;
